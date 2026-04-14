181df171152518c80e696b5c174176863b0f5c16
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2026-04-14 15:44:15 +0530

    learn card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+  return(
+    <div className="card">
+      <img src="https://placehold.co/150"></img>
+      <h2>John Doe</h2>
+      <p>Software Engineer</p>
+    </div>
+  );
+}
+
+export default Card;
