e7db7b43878cf262a3fa9b709bc164ef26433242
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2026-04-14 17:08:53 +0530

    learn click events

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+function Button(){
+  const handleClick = (e) => {
+    e.target.textContent = "OUCH!";
+  }
+  return(
+    <>
+      <button onDoubleClick={(e) => handleClick(e)}>
+        Click Me
+      </button>
+    </>
+  );
+}
+
+export default Button
