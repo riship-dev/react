0f54da8078f425ec34d71ab8013ec4ac81e4f351
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2026-04-14 16:45:52 +0530

    learn list rendering

---
+++ src/components/List.jsx
@@ -0,0 +1,19 @@
+function List(props) {
+  
+  const itemList = props.items;
+
+  const listItems = itemList.map(item => 
+    <li key={item.id}>
+      {item.name}: &nbsp;
+      <b>{item.calories}</b>
+    </li>
+  );
+
+  return(
+    <ol>
+      {listItems}
+    </ol>
+  );
+}
+
+export default List;
